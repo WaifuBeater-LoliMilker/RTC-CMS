016edb3b0b687850eca78de88e1d1fe5478c9522
Waifu Beater <tech20@rtc.edu.vn>
2025-05-20 16:30:00 +0700

    add fontawesome library and finish login template

---
+++ RTC-CMS/wwwroot/js/Auth/login.js
@@ -0,0 +1,43 @@
+﻿window.addEventListener('DOMContentLoaded', event => {
+    const form = document.getElementById('form_login')
+    const errorText = document.getElementsByClassName('text-error')[0]
+    const showPass = document.getElementsByClassName('show-password')[0]
+
+    form.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        errorText.textContent = '';
+        const formData = new FormData(form);
+        const data = new URLSearchParams(formData);
+        try {
+            const response = await fetch(form.action, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+            if (response.ok) {
+                window.location.href = '/home';
+            } else {
+                const message = await response.text();
+                errorText.textContent = message || 'Đã có lỗi xảy ra, vui lòng thử lại'
+            }
+        }
+        catch (err) {
+            console.log(err)
+            errorText.textContent = 'Đã có lỗi xảy ra, vui lòng thử lại';
+        }
+    })
+    showPass.addEventListener('click', function (e) {
+        e.preventDefault();
+        const pwInput = document.getElementById('password');
+        const pwBtn = showPass.getElementsByTagName('i')[0];
+        if (pwInput.type == 'password') {
+            pwInput.type = 'text';
+            pwBtn.className = 'fa-solid fa-eye-slash'
+        } else {
+            pwInput.type = 'password';
+            pwBtn.className = 'fa-solid fa-eye'
+        }
+    })
+});
