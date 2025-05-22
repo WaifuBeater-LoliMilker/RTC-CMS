6abc8b5784dc52410ce6b38cd6db431c4c4cc9c4
huadang27 <78135100+huadang27@users.noreply.github.com>
2025-05-22 16:01:34 +0700

    create api get machine

---
+++ RTC-CMS/Controllers/MachineAPIController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RTC_CMS.Models;
+
+namespace RTC_CMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineAPIController : ControllerBase
+    {
+        private readonly IGenericRepo _genericRepo;
+
+        public MachineAPIController(IGenericRepo genericRepo)
+        {
+            _genericRepo = genericRepo;
+        }
+
+        [HttpGet("GetAllMachines")]
+        public async Task<IActionResult> GetAllMachines()
+        {
+            var machines = await _genericRepo.GetAll<Machines>();
+            return Ok(machines);
+        }
+           
+
+    }
+}
