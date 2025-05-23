2f798c3db3bec7b63599d400e5e0dc2a5f73db5f
Waifu Beater <tech20@rtc.edu.vn>
2025-05-23 10:41:25 +0700

    restore authcontroller

---
+++ RTC-CMS/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RTC_CMS.Common;
+using RTC_CMS.Models;
+using System.Security.Claims;
+
+namespace RTC_CMS.Controllers
+{
+    [Route("")]
+    public class AuthController(IGenericRepo repo) : Controller
+    {
+        private IGenericRepo _repo = repo;
+
+        [HttpGet("login")]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromForm] string username, [FromForm] string password)
+        {
+            try
+            {
+                var user = await _repo.FindModel<Users>
+                    (u => u.UserName == username && u.Password == password);
+                if (user == null) return Unauthorized("Sai tên đăng nhập hoặc mật khẩu");
+                var claims = new List<Claim> {
+                    new (ClaimTypes.Name, user.UserName),
+                    new (ClaimTypes.Role, user.RoleId.ToString()!)
+                };
+                var identity = new ClaimsIdentity(claims, "RTC_CMS_Cookie"); //cookie name from Program.cs
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync("RTC_CMS_Cookie", principal);
+                HttpContext.Session.SetObject<Users>("User", user);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest("Đã có lỗi xảy ra, vui lòng thử lại");
+            }
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("RTC_CMS_Cookie");
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet("unauthorized")]
+        [AllowAnonymous]
+        public IActionResult UnauthorizedView()
+        {
+            return View();
+        }
+    }
+}
